827dc31bd7313c172cb4e8f0351708d324a39962
jbisne <jisun7894@gmail.com>
2019-05-17 14:47:33 +0900

    2019-05-17 커밋1

---
+++ 06.Spring Boot/Ex27_Security_Database/src/main/java/com/study/springboot/Ex27SecurityDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex27SecurityDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex27SecurityDatabaseApplication.class, args);
+	}
+
+}
